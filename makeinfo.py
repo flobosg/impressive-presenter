fa9df7b6a76440ca172a066c0dc40cc9c6a8c148
Francisco Lobos González <flobosg@gmail.com>
2013-05-15 03:24:08 -0400

    Pythonic file handling.

--- makeinfo.py
+++ makeinfo.py
@@ -6,27 +6,25 @@ filename = argv[1]
 
 document = PdfFileReader(file(filename, "rb"))
 pages = document.getNumPages()
-out = open(filename+".info", 'w')
 
-out.write("""import json
+with open(filename+".info", 'w') as out:
+    out.write("""import json
 
-def UpdateInfo():
-    global FileName, FileList, PageCount
-    global DocumentTitle
-    global Pcurrent, Pnext, Tcurrent, Tnext, InitialPage
-    global RTrunning, RTrestart, StartTime, PageEnterTime, CurrentTime
+    def UpdateInfo():
+        global FileName, FileList, PageCount
+        global DocumentTitle
+        global Pcurrent, Pnext, Tcurrent, Tnext, InitialPage
+        global RTrunning, RTrestart, StartTime, PageEnterTime, CurrentTime
 
-    io = open('"""+path.dirname(filename)+"""./json.txt', 'w')
-    json.dump(({"page_count": PageCount, "current_page": Pcurrent, "previous_page": Pnext, "start_time": StartTime, "pageenter_time": PageEnterTime, "current_time": CurrentTime, "notes": PageProps[Pcurrent]['notes']}), io)
-    io.close()
+        io = open('"""+path.dirname(filename)+"""./json.txt', 'w')
+        json.dump(({"page_count": PageCount, "current_page": Pcurrent, "previous_page": Pnext, "start_time": StartTime, "pageenter_time": PageEnterTime, "current_time": CurrentTime, "notes": PageProps[Pcurrent]['notes']}), io)
+        io.close()
 
-PageProps = {
-""")
+    PageProps = {
+    """)
 
-for i in range(1,pages + 1):
-    if i < pages:
-        out.write("\t"+str(i)+": {\n\t\t'transition': None,\n\t\t'overview': True,\n\t\t'notes': '',\n\t\t'OnEnter': UpdateInfo\n\t},\n")
-    else:
-        out.write("\t"+str(i)+": {\n\t\t'transition': None,\n\t\t'overview': True,\n\t\t'notes': '',\n\t\t'OnEnter': UpdateInfo\n\t}\n}")
-
-out.close()
+    for i in range(1,pages + 1):
+        if i < pages:
+            out.write("\t"+str(i)+": {\n\t\t'transition': None,\n\t\t'overview': True,\n\t\t'notes': '',\n\t\t'OnEnter': UpdateInfo\n\t},\n")
+        else:
+            out.write("\t"+str(i)+": {\n\t\t'transition': None,\n\t\t'overview': True,\n\t\t'notes': '',\n\t\t'OnEnter': UpdateInfo\n\t}\n}")
